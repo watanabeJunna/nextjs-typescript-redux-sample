b11d432d7bdb382d2a6aafed8627156a33a62495
watanabeJunna <junna7897@icloud.com>
2019-11-12 20:41:14 +0900

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,39 @@
-import styled from 'styled-components'
+import styled, { ThemeProvider, createGlobalStyle, GlobalStyleComponent } from 'styled-components'
+import reset from 'styled-reset'
 
 export default () => {
+    const GlobalStyle: GlobalStyleComponent<{}, {}> = createGlobalStyle`
+        ${reset}
+        body, html {
+            font-size: 1.1em;
+            margin: 30px 0;
+        }
+    `
+
+    const theme: any = {
+        fontFamily: "'Raleway', sans-serif",
+        color: "#5f6f81",
+    }
+
     const Wrapper = styled.div`
         width: 80%;
+        padding: 32px 0;
         margin: 0 auto;
-        padding: 0;
-        color: #5f6f81;
-        font-size: 1.9em;
-        font-family: 'Raleway', sans-serif;
-        padding: 18px 32px 18px 0;
+        color: ${props => props.theme.color};
         border-bottom: 1px solid #afafbc;
         margin-bottom: 50px;
+        font-size: 1.9em;
+        font-family: ${props => props.theme.fontFamily};
     `
+
     return (
         <>
-            <Wrapper>Counter App</Wrapper>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Wrapper>
+                    <p>Counter App</p>
+                </Wrapper>
+            </ThemeProvider>
         </>
     )
 }
