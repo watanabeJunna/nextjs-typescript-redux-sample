1887f8cc31143ee24739edeff5fc406fe29c6ed1
watanabeJunna <junna7897@icloud.com>
2019-11-13 16:26:26 +0900

    inject initial value

--- store/reducer.ts
+++ store/reducer.ts
@@ -1,9 +1,27 @@
+import uuid from 'uuid/v4'
 import { combineReducers } from 'redux'
 import * as Todos from './todos'
+import { Todo, State } from './todos'
+
+/// DEBUG
+const inject: State = {
+    todos: [
+        'Export Component完成させたい',
+        '一通り突貫したテストをしたい',
+        'DataContentコンポーネントのレンダリングを効率化したい',
+        'connectを導入したい、Reduxのやつ',
+    ].map((e: string) => {
+        return {
+            id: uuid(),
+            done: false,
+            task: e
+        } as Todo
+    })
+}
 
 export const initialState = () => {
     return {
-        todos: Todos.initialState(),
+        todos: Todos.initialState(inject),
     }
 }
 
