bb469ae25d6195a42ae727e7ae2cf54b8b8059b6
watanabeJunna <junna7897@icloud.com>
2019-11-13 16:25:02 +0900

    refactor for _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import App, { Container, AppContext } from 'next/app'
+import App, { AppContext } from 'next/app'
 import withRedux from 'next-redux-wrapper'
 import { initStore, ReduxStoreInstance } from '../store'
 
@@ -11,24 +11,20 @@ type Props = {
 export default withRedux(initStore)(
     class extends App<Props> {
         static async getInitialProps({ Component, ctx }: AppContext) {
-            let pageProps = {}
-
-            if (Component.getInitialProps) {
-                pageProps = await Component.getInitialProps(ctx)
+            return {
+                pageProps: {
+                    ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {})
+                }
             }
-
-            return { pageProps }
         }
 
         render() {
             const { Component, pageProps, store } = this.props
 
             return (
-                <Container>
-                    <Provider store={store}>
-                        <Component {...pageProps} />
-                    </Provider>
-                </Container>
+                <Provider store={store}>
+                    <Component {...pageProps} />
+                </Provider>
             )
         }
     }
